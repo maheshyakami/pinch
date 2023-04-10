af5c3ed576c280829c3b0d97065f1bdd1af3fd19
Mahesh Yakami <yakami.mahesh@gmail.com>
2023-04-10 09:02:01 +0545

    init project

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Mahesh Yakami on 10/04/2023.
+//  
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
